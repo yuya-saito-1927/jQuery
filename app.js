6926c2f6c01da514a192ab41c81c43fbcb79a344
齊藤有哉 <yuyasaito@saitouyuukanas-MacBook-Pro.local>
2021-05-30 23:20:05 +0900

    [JS_7]finish

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-// $(function(){
-//   $('.box1').mouseover(function(){
-//     $('.box1').css({'background-color': '#0000ff'});
-//   });
-//   $('.box1').mouseout(function(){
-//     $('.box1').css({'background-color': '#ff0000'});
-//   });
-// });
-
 $(function(){
-  $('.box1').on('click', function(){
-    $('.box1').addClass('box1-ext')
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext')
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
